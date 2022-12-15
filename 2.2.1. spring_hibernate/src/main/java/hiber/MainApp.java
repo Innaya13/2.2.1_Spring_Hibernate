b67f1e4cca5117862fdf061ba2caba96e58159c0
mac <innakar@gmail.com>
2022-12-15 17:17:43 -0500

    hope it's ok

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,15 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("Valera", "Bogatyr", "valera@mail.ru",new Car("BTR", 1)));
+      userService.add(new User("Oleg", "Sokol", "oleg@mail.ru",new Car("TANK", 2)));
+      userService.add(new User("Bat", "Man", "batman@mail.ru",new Car("BATMOBILE", 3)));
+      userService.add(new User("Valentina", "Koroleva", "valya@mail.ru",new Car("TRACTOR", 4)));
+
+      //      Car car1=new Car("BTR",1);
+      //      Car car2=new Car("TANK",2);
+      //      Car car3=new Car("BATMOBILE",3);
+      //      Car car4=new Car("TRACTOR",4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +32,9 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println(user.getCar());
       }
-
+      System.out.println(userService.userCar("TANK", 2));
       context.close();
    }
 }
